eca8b5edafe64abca1fe3a61097f7be15868cdf1
umerprogrammer <umer.programmar@example.com>
2024-02-17 10:00:12 +0500

    17Feb2024 New Data

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var btnAdd = document.getElementById("btntodo");
 var txtTodo = document.getElementById("txttodo");
 
-var data = [];
-
+let data = new Array();
 
 
 function addTodo() {
-    if(txtTodo.value == "") {
+    if (txtTodo.value == "") {
         alert("Please enter todo");
         return;
     }
-    var id =  data == null ? 1 : data.length +1;
+    var id = data == null ? 1 : data.length + 1;
     todoObj = {
         "id": id,
         "description": txtTodo.value.toString(),
@@ -21,77 +20,81 @@ function addTodo() {
     if (data.length > 0)
         addList();
 
-        txtTodo.value = "";
+    txtTodo.value = "";
 
 }
 
 function addList() {
 
+    var storageData = JSON.parse(localStorage.getItem("myTodo"));
+    if (storageData != null) {
+        data = storageData;
+    }
 
     var tbody = document.getElementById("tableBody");
     while (tbody.firstChild) {
         tbody.removeChild(tbody.firstChild);
     }
-data = JSON.parse( localStorage.getItem("myTodo"));
+    if (data == null) return;
+    if (data.length > 0) {
+        data.forEach(element => {
+            var newRow = document.createElement("tr");
+            var id = document.createElement("td");
+            var descr = document.createElement("td");
+            var status = document.createElement("td");
+            var close = document.createElement("td");
+            var btnstatus = document.createElement("input");
+            var btnclose = document.createElement("input");
+
+            id.textContent = element.id;
+            descr.textContent = element.description;
+
+            btnstatus.setAttribute("type", "button");
+            btnstatus.setAttribute("value", element.todoStatus);
+            btnstatus.setAttribute("id", "btnstatus");
+            btnstatus.setAttribute("class", "btnstatus");
+            btnstatus.setAttribute("onclick", "markStatus(this)");
+
+            btnclose.setAttribute("type", "button");
+            btnclose.setAttribute("value", "Delete");
+            btnclose.setAttribute("id", "btnclose");
+            btnclose.setAttribute("class", "btnclose");
+            btnclose.setAttribute("onclick", "deleteTodo(this)");
 
 
-    data.forEach(element => {
-        var newRow = document.createElement("tr");
-        var id = document.createElement("td");
-        var descr = document.createElement("td");
-        var status = document.createElement("td");
-        var close = document.createElement("td");
-        var btnstatus = document.createElement("input");
-        var btnclose = document.createElement("input");
+            status.appendChild(btnstatus);
+            close.appendChild(btnclose);
 
-        id.textContent = element.id;
-        descr.textContent = element.description;
-        
-        btnstatus.setAttribute("type","button");
-        btnstatus.setAttribute("value",element.todoStatus);
-        btnstatus.setAttribute("id","btnstatus");
-        btnstatus.setAttribute("class","btnstatus");
-        btnstatus.setAttribute("onclick","markStatus(this)");
+            newRow.appendChild(id);
+            newRow.appendChild(descr);
+            newRow.appendChild(status);
+            newRow.appendChild(close);
 
-        btnclose.setAttribute("type","button");
-        btnclose.setAttribute("value","Delete");
-        btnclose.setAttribute("id","btnclose");
-        btnclose.setAttribute("class","btnclose");
-        btnclose.setAttribute("onclick","deleteTodo(this)");
-
-
-        status.appendChild (btnstatus);
-        close.appendChild (btnclose);
-
-        newRow.appendChild(id);
-        newRow.appendChild(descr);
-        newRow.appendChild(status);
-        newRow.appendChild(close);
-    
-        newRow.setAttribute("id",element.id);
-        tbody.appendChild(newRow);
-     });
+            newRow.setAttribute("id", element.id);
+            tbody.appendChild(newRow);
+        });
+    }
 
 }
 
-function deleteTodo(ele){
+function deleteTodo(ele) {
     var tbody = document.getElementById("tableBody");
     var element = ele.parentElement;
 
-    var trIndex = data.findIndex(elm => elm.id ==element.parentElement.id );
+    var trIndex = data.findIndex(elm => elm.id == element.parentElement.id);
 
-    data.splice(trIndex,1);
+    data.splice(trIndex, 1);
     tbody.removeChild(element.parentElement);
 
     localStorage.removeItem("myTodo");
     localStorage.setItem("myTodo", JSON.stringify(data));
 }
 
-function markStatus(ele){
+function markStatus(ele) {
     var tbody = document.getElementById("tableBody");
     var element = ele.parentElement;
 
-    var trIndex = data.findIndex(elm => elm.id ==element.parentElement.id );
+    var trIndex = data.findIndex(elm => elm.id == element.parentElement.id);
     data[trIndex].todoStatus = "Completed";
     console.log(data);
     localStorage.removeItem("myTodo");
